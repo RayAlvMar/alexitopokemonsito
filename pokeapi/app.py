b1f8d64d16b4a3bfdf5d9ad90072138543849b4b
RayAlvMar <24308060610026@cetis61.edu.mx>
2025-11-13 08:04:25 -0600

    hjkgk

--- pokeapi/app.py
+++ pokeapi/app.py
@@ -30,7 +30,15 @@ def search_pokemon():
                 'weight': pokemon_data['weight'] / 10,
                 'image': pokemon_data['sprites']['front_default'],
                 'types': [t['type']['name'].title() for t in pokemon_data['types']]
+                'abilities': [a['ability']['name'].title() for a in pokemon_data['abilities']]
             }
         return render_template('pokemon.html', pokemon=pokemon_data)
+    else:
+        flash(f'Pokémon '{pokemon}" no encontrado.", 'error')
+        return redirect(url_for('index'))
+    except request.exceptions.RequestException
+    flash('Error al conectar con la API de Pokémon', 'error')
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
